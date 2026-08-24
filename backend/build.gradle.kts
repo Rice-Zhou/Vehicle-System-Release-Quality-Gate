bdf104b694015e31e2ebd70a353d13a155e94fae
uidq4225 <Heqi.Zhou@desaysv.com>
2026-08-24 15:36:10 +0800

    build(m1): establish backend skeleton

---
+++ backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+}
+
+group = "com.ricezhou.vsrqg"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ backend/src/main/kotlin/com/ricezhou/vsrqg/VsrqgApplication.kt
@@ -0,0 +1,11 @@
+package com.ricezhou.vsrqg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VsrqgApplication
+
+fun main(args: Array<String>) {
+    runApplication<VsrqgApplication>(*args)
+}
