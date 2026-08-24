6a7c706e049295059850b1b3d364f9ec91c7e613
uidq4225 <Heqi.Zhou@desaysv.com>
2026-08-24 15:36:10 +0800

    build(m1): establish backend skeleton

---
+++ backend/src/test/kotlin/com/ricezhou/vsrqg/ApplicationContextTest.kt
@@ -0,0 +1,10 @@
+package com.ricezhou.vsrqg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationContextTest {
+    @Test
+    fun `context loads`() = Unit
+}
